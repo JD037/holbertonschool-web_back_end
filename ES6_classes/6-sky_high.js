28c01a9f0478bd3163f70e5f342e0765866a435b
JoshD <joj237@gmail.com>
2023-09-08 00:39:37 +0000

    Implemented SkyHighBuilding class with inheritance and method override for evacuation message.

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,18 @@
+// 6-sky_high.js
+/* eslint-disable no-underscore-dangle */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft); // Passing sqft to the parent class' constructor
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
