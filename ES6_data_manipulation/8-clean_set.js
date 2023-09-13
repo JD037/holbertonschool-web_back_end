27d977f3321ed4c09448d177a864e525fe41f712
JoshD <joj237@gmail.com>
2023-09-13 20:34:34 +0000

    fix output by rming slicing

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -3,9 +3,9 @@ export default function cleanSet(set, startString) {
 
   let result = '';
   for (const item of set) {
-    if (item.startsWith(startString)) {
+    if (item && item.startsWith(startString)) {
       result += `${item.replace(startString, '')}-`;
     }
   }
-  return result.slice(0, -1); // Remove the last '-'
+  return result;
 }
