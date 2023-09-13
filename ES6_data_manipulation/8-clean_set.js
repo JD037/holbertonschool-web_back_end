3077d95737ba66c11e1f786dd19b42caa26900f2
JoshD <joj237@gmail.com>
2023-09-13 20:42:06 +0000

    fix lint

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,11 @@
 export default function cleanSet(set, startString) {
-    if (!startString || startString.length === 0) return '';
-  
-    let result = '';
-    for (const item of set) {
-      if (item.startsWith(startString)) {
-        result += item.replace(startString, '') + '-';
-      }
+  if (!startString || startString.length === 0) return '';
+
+  let result = '';
+  for (const item of set) {
+    if (item.startsWith(startString)) {
+      result += `${item.replace(startString, '')}-`;
     }
-    return result.slice(0, -1);  // Remove the last '-'
   }
+  return result.slice(0, -1); // Remove the last '-'
+}
