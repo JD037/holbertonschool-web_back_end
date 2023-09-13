d22c2e221682d70bbb5e3941a3ef60939cd1c48d
JoshD <joj237@gmail.com>
2023-09-13 19:47:15 +0000

    rm unused vars

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
+  return new Promise(() => {
     // You can resolve or reject the promise here based on some conditions.
     // For this task, we don't need to specify any conditions.
   });
