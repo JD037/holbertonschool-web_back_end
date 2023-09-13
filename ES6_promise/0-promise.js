ebf900c44db88c2cf14eb2af3406cf397bb6de10
JoshD <joj237@gmail.com>
2023-09-13 19:27:54 +0000

    Created a function that returns a new Promise.

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,6 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // You can resolve or reject the promise here based on some conditions.
+    // For this task, we don't need to specify any conditions.
+  });
+}
